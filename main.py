2633894c2db6b912299187cc5b03521a37ad0c38
henriklenning <henrik.fohrenbachlenning@gmail.com>
2026-03-31 22:12:26 +0200

    Add initial FastAPI application and requirements

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# The Manifest is the "ID Card" of your Stremio Addon
+MANIFEST = {
+    "id": "org.socialreviews.example",
+    "name": "Friend Reviews",
+    "version": "0.0.1",
+    "description": "See what your friends are saying about movies.",
+    "resources": ["meta"], # This tells Stremio we provide extra info
+    "types": ["movie", "series"],
+    "catalogs": []
+}
+
+@app.get("/manifest.json")
+async def get_manifest():
+    return MANIFEST
+
+# A placeholder for where the reviews will go later
+@app.get("/meta/{type}/{id}.json")
+async def get_meta(type: str, id: str):
+    return {
+        "meta": {
+            "id": id,
+            "description": "Friend Review: 'This was a great movie!' - @UserA"
+        }
+    }
